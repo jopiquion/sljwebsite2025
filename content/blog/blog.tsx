3c6c96032d31ebf7317adf3fc178a64f0ff97524
Joschel Piquion <jopiquion@gmail.com>
2025-06-10 13:25:36 -0500

    added blog content page

---
+++ content/blog/blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Stackbit Next.js Site!</h1>
+      <p>This is the homepage. Content will eventually come from content/pages/index.md</p>
+    </div>
+  );
+};
+
+export default Blog;
